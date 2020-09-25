12f7e6c8907579f628120f39fd2b084449b37ab9
Hyeonsol <soline013@naver.com>
2020-09-25 21:44:19 +0900

    Create 모두를 위한 딥러닝 LAB09.py

---
+++ 모두를 위한 딥러닝/모두를 위한 딥러닝 LAB09.py
@@ -0,0 +1,255 @@
+%tensorflow_version 1.x
+import tensorflow as tf
+
+import tensorflow.compat.v1 as tf
+import numpy as np
+tf.disable_v2_behavior()
+
+tf.set_random_seed(777)
+
+
+
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 1])
+W = tf.Variable(tf.random_normal([2, 1]), name="weight")
+b = tf.Variable(tf.random_normal([1]), name="bias")
+
+#Sigmoid Function.
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+#Different Cost Function.
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        _, cost_val, w_val = sess.run(
+                  [train, cost, W], feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print("Step:", step, "Cost:", cost_val, "\n", w_val)
+
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
+'''
+Step: 10000 Cost: 0.6931472 
+ [[1.3297395e-07]
+ [1.3354921e-07]]
+
+Hypothesis:  [[0.5]
+ [0.5]
+ [0.5]
+ [0.5]] 
+Correct:  [[0.]
+ [0.]
+ [0.]
+ [0.]] 
+Accuracy:  0.5
+'''
+
+
+
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+#1st.
+W1 = tf.Variable(tf.random_normal([2, 2]), name='weight1')
+b1 = tf.Variable(tf.random_normal([2]), name='bias1')
+layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+
+#2rd.
+W2 = tf.Variable(tf.random_normal([2, 1]), name='weight2')
+b2 = tf.Variable(tf.random_normal([1]), name='bias2')
+hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2) #layer1 사용.
+
+#Different Cost Function.
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(10001):
+        _, cost_val = sess.run([train, cost], feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print("Step:", step, "Cost:", cost_val)
+
+    h, p, a = sess.run(
+        [hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print(f"\nHypothesis:\n{h} \nPredicted:\n{p} \nAccuracy:\n{a}")
+'''
+Step: 9600 Cost: 0.014685788
+Step: 9700 Cost: 0.014466861
+Step: 9800 Cost: 0.01425423
+Step: 9900 Cost: 0.014047621
+Step: 10000 Cost: 0.013846756
+
+Hypothesis:
+[[0.01338217]
+ [0.98166394]
+ [0.98809415]
+ [0.011358  ]] 
+Predicted:
+[[0.]
+ [1.]
+ [1.]
+ [0.]] 
+Accuracy:
+1.0
+'''
+
+
+
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+#1 ~ 4까지 점점 쌓여감.
+W1 = tf.Variable(tf.random_normal([2, 10]), name='weight1')
+b1 = tf.Variable(tf.random_normal([10]), name='bias1')
+layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+W2 = tf.Variable(tf.random_normal([10, 10]), name='weight2')
+b2 = tf.Variable(tf.random_normal([10]), name='bias2')
+layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+W3 = tf.Variable(tf.random_normal([10, 10]), name='weight3')
+b3 = tf.Variable(tf.random_normal([10]), name='bias3')
+layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
+W4 = tf.Variable(tf.random_normal([10, 1]), name='weight4')
+b4 = tf.Variable(tf.random_normal([1]), name='bias4')
+hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+
+#Different Cost Function.
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(10001):
+        _, cost_val = sess.run([train, cost], feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print("Step:", step, "Cost:", cost_val)
+
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
+'''
+Step: 9600 Cost: 0.0013945088
+Step: 9700 Cost: 0.0013739156
+Step: 9800 Cost: 0.0013539495
+Step: 9900 Cost: 0.0013344167
+Step: 10000 Cost: 0.001315481
+
+Hypothesis:  [[0.00103005]
+ [0.9987619 ]
+ [0.99876404]
+ [0.00175358]] 
+Correct:  [[0.]
+ [1.]
+ [1.]
+ [0.]] 
+Accuracy:  1.0
+'''
+
+
+
+x_data = np.array([[0., 0.], [0., 1.], [1., 0.], [1., 1.]], dtype=np.float32)
+y_data = np.array([[0.], [1.], [1.], [0.]], dtype=np.float32)
+X = tf.placeholder(tf.float32, shape=[None, 2], name="x")
+Y = tf.placeholder(tf.float32, shape=[None, 1], name="y")
+
+with tf.name_scope("Layer1"):
+    W1 = tf.Variable(tf.random_normal([2, 2]), name="weight_1")
+    b1 = tf.Variable(tf.random_normal([2]), name="bias_1")
+    layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+
+    tf.summary.histogram("W1", W1)
+    tf.summary.histogram("b1", b1)
+    tf.summary.histogram("Layer1", layer1)
+
+
+with tf.name_scope("Layer2"):
+    W2 = tf.Variable(tf.random_normal([2, 1]), name="weight_2")
+    b2 = tf.Variable(tf.random_normal([1]), name="bias_2")
+    hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+    tf.summary.histogram("W2", W2)
+    tf.summary.histogram("b2", b2)
+    tf.summary.histogram("Hypothesis", hypothesis)
+
+# cost/loss function
+with tf.name_scope("Cost"):
+    cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+    tf.summary.scalar("Cost", cost)
+
+with tf.name_scope("Train"):
+    train = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+tf.summary.scalar("accuracy", accuracy)
+
+# Launch graph
+with tf.Session() as sess:
+    # tensorboard --logdir=./logs/xor_logs
+    merged_summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs/xor_logs_r0_01")
+    writer.add_graph(sess.graph)  # Show the graph
+
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        _, summary, cost_val = sess.run(
+            [train, merged_summary, cost], feed_dict={X: x_data, Y: y_data}
+        )
+        writer.add_summary(summary, global_step=step)
+
+        if step % 100 == 0:
+            print(step, cost_val)
+
+    # Accuracy report
+    h, p, a = sess.run(
+        [hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data}
+    )
+    
+    print(f"\nHypothesis:\n{h} \nPredicted:\n{p} \nAccuracy:\n{a}")
+'''
+9600 7.134933e-05
+9700 6.786221e-05
+9800 6.4539025e-05
+9900 6.136488e-05
+10000 5.8384467e-05
+
+Hypothesis:
+[[6.1310086e-05]
+ [9.9993694e-01]
+ [9.9995077e-01]
+ [5.9751303e-05]] 
+Predicted:
+[[0.]
+ [1.]
+ [1.]
+ [0.]] 
+Accuracy:
+1.0
+'''
+
+%load_ext tensorboard
+
+#./logs/xor_logs_r0_01로 쓰이므로 --logdir= 옵션으로 Tensorboard를 실행한다.
+%tensorboard --logdir=./logs/xor_logs_r0_01
