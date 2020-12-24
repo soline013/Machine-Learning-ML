21cfa5e8dda4a29b5839d18fd9a8bcafb28774e0
Hyeonsol <soline013@naver.com>
2020-12-24 23:38:00 +0900

    Update 모두를 위한 딥러닝 LAB11.py

--- 모두를 위한 딥러닝/모두를 위한 딥러닝 LAB11.py
+++ 모두를 위한 딥러닝/모두를 위한 딥러닝 LAB11.py
@@ -14,11 +14,11 @@ image = np.array([[[[1],[2],[3]],
                    [[7],[8],[9]]]], dtype=np.float32)
 print(image.shape)
 plt.imshow(image.reshape(3,3), cmap='Greys') #.imshow()
-```
+'''
 (1, 3, 3, 1)
 <matplotlib.image.AxesImage at 0x7f11ad5e3a90>
 ![image](https://user-images.githubusercontent.com/66259854/103092365-5733eb80-463a-11eb-823e-23ccd430556f.png)
-```
+'''
 
 print("image.shape", image.shape)
 weight = tf.constant([[[[1.]],[[1.]]],
@@ -31,14 +31,14 @@ conv2d_img = np.swapaxes(conv2d_img, 0, 3) #.swapaxes()
 for i, one_img in enumerate(conv2d_img):
     print(one_img.reshape(2,2))
     plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(2,2), cmap='gray') #.subplot()
-```
+'''
 image.shape (1, 3, 3, 1)
 weight.shape (2, 2, 1, 1)
 conv2d_img.shape (1, 2, 2, 1)
 [[12. 16.]
  [24. 28.]]
 ![image](https://user-images.githubusercontent.com/66259854/103092362-5438fb00-463a-11eb-8bb6-eed6ced385d5.png)
-```
+'''
 
 print("image.shape", image.shape)
 
@@ -52,7 +52,7 @@ conv2d_img = np.swapaxes(conv2d_img, 0, 3)
 for i, one_img in enumerate(conv2d_img):
     print(one_img.reshape(3,3))
     plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(3,3), cmap='gray')
-```
+'''
 image.shape (1, 3, 3, 1)
 weight.shape (2, 2, 1, 1)
 conv2d_img.shape (1, 3, 3, 1)
@@ -60,7 +60,7 @@ conv2d_img.shape (1, 3, 3, 1)
  [24. 28. 15.]
  [15. 17.  9.]]
 ![image](https://user-images.githubusercontent.com/66259854/103092370-5b600900-463a-11eb-931c-567f71b1bba9.png)
-```
+'''
 
 print("image.shape", image.shape)
 weight = tf.constant([[[[1.,10.,-1.]],[[1.,10.,-1.]]],[[[1.,10.,-1.]],[[1.,10.,-1.]]]])
@@ -72,7 +72,7 @@ conv2d_img = np.swapaxes(conv2d_img, 0, 3)
 for i, one_img in enumerate(conv2d_img):
     print(one_img.reshape(3,3))
     plt.subplot(1,3,i+1), plt.imshow(one_img.reshape(3,3), cmap='gray')
-```
+'''
 image.shape (1, 3, 3, 1)
 weight.shape (2, 2, 1, 3)
 conv2d_img.shape (1, 3, 3, 3)
@@ -86,7 +86,7 @@ conv2d_img.shape (1, 3, 3, 3)
  [-24. -28. -15.]
  [-15. -17.  -9.]]
 ![image](https://user-images.githubusercontent.com/66259854/103092375-5dc26300-463a-11eb-8ecc-46ed6cfd33f3.png)
-```
+'''
 
 image = np.array([[[[4],[3]],
                     [[2],[1]]]], dtype=np.float32)
@@ -94,10 +94,10 @@ pool = tf.nn.max_pool(image, ksize=[1, 2, 2, 1], #.nn.max_pool()
                     strides=[1, 1, 1, 1], padding='VALID')
 print(pool.shape)
 print(pool.eval())
-```
+'''
 (1, 1, 1, 1)
 [[[[4.]]]]
-```
+'''
 
 image = np.array([[[[4],[3]],
                     [[2],[1]]]], dtype=np.float32)
@@ -105,11 +105,175 @@ pool = tf.nn.max_pool(image, ksize=[1, 2, 2, 1],
                     strides=[1, 1, 1, 1], padding='SAME')
 print(pool.shape)
 print(pool.eval())
-```
+'''
 (1, 2, 2, 1)
 [[[[4.]
    [3.]]
 
   [[2.]
    [1.]]]]
-```
+'''
+
+
+
+import tensorflow as tf
+import random
+# import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(777)  # reproducibility
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# Check out https://www.tensorflow.org/get_started/mnist/beginners for
+# more information about the mnist dataset
+
+# hyper parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# dropout (keep_prob) rate  0.7~0.5 on training, but should be 1 for testing
+keep_prob = tf.placeholder(tf.float32)
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])   # img 28x28x1 (black/white)
+Y = tf.placeholder(tf.float32, [None, 10])
+
+# L1 ImgIn shape=(?, 28, 28, 1)
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+#    Conv     -> (?, 28, 28, 32)
+#    Pool     -> (?, 14, 14, 32)
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
+'''
+Tensor("Conv2D:0", shape=(?, 28, 28, 32), dtype=float32)
+Tensor("Relu:0", shape=(?, 28, 28, 32), dtype=float32)
+Tensor("MaxPool:0", shape=(?, 14, 14, 32), dtype=float32)
+Tensor("dropout/mul:0", shape=(?, 14, 14, 32), dtype=float32)
+'''
+
+# L2 ImgIn shape=(?, 14, 14, 32)
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+#    Conv      ->(?, 14, 14, 64)
+#    Pool      ->(?, 7, 7, 64)
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+'''
+Tensor("Conv2D_1:0", shape=(?, 14, 14, 64), dtype=float32)
+Tensor("Relu_1:0", shape=(?, 14, 14, 64), dtype=float32)
+Tensor("MaxPool_1:0", shape=(?, 7, 7, 64), dtype=float32)
+Tensor("dropout_1/mul:0", shape=(?, 7, 7, 64), dtype=float32)
+'''
+
+# L3 ImgIn shape=(?, 7, 7, 64)
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+#    Conv      ->(?, 7, 7, 128)
+#    Pool      ->(?, 4, 4, 128)
+#    Reshape   ->(?, 4 * 4 * 128) # Flatten them for FC
+L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[
+                    1, 2, 2, 1], padding='SAME')
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+L3_flat = tf.reshape(L3, [-1, 128 * 4 * 4])
+'''
+Tensor("Conv2D_2:0", shape=(?, 7, 7, 128), dtype=float32)
+Tensor("Relu_2:0", shape=(?, 7, 7, 128), dtype=float32)
+Tensor("MaxPool_2:0", shape=(?, 4, 4, 128), dtype=float32)
+Tensor("dropout_2/mul:0", shape=(?, 4, 4, 128), dtype=float32)
+Tensor("Reshape_1:0", shape=(?, 2048), dtype=float32)
+'''
+
+# L4 FC 4x4x128 inputs -> 625 outputs
+W4 = tf.get_variable("W4", shape=[128 * 4 * 4, 625],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([625]))
+L4 = tf.nn.relu(tf.matmul(L3_flat, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+'''
+Tensor("Relu_3:0", shape=(?, 625), dtype=float32)
+Tensor("dropout_3/mul:0", shape=(?, 625), dtype=float32)
+'''
+
+# L5 Final FC 625 inputs -> 10 outputs
+W5 = tf.get_variable("W5", shape=[625, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+logits = tf.matmul(L4, W5) + b5
+'''
+Tensor("add_1:0", shape=(?, 10), dtype=float32)
+'''
+
+# define cost/loss & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# initialize
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# train my model
+print('Learning started. It takes sometime.')
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+# Test model and check accuracy
+
+# if you have a OOM error, please refer to lab-11-X-mnist_deep_cnn_low_memory.py
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
+
+# Get one and predict
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(logits, 1), feed_dict={X: mnist.test.images[r:r + 1], keep_prob: 1}))
+
+# plt.imshow(mnist.test.images[r:r + 1].
+#           reshape(28, 28), cmap='Greys', interpolation='nearest')
+# plt.show()
+'''
+Learning started. It takes sometime.
+Epoch: 0001 cost = 0.365078180
+Epoch: 0002 cost = 0.102141358
+Epoch: 0003 cost = 0.074173531
+Epoch: 0004 cost = 0.060685895
+Epoch: 0005 cost = 0.050624732
+Epoch: 0006 cost = 0.046288653
+Epoch: 0007 cost = 0.042086853
+Epoch: 0008 cost = 0.039240586
+Epoch: 0009 cost = 0.036376112
+Epoch: 0010 cost = 0.035613905
+Epoch: 0011 cost = 0.028902437
+Epoch: 0012 cost = 0.030085018
+Epoch: 0013 cost = 0.027165487
+Epoch: 0014 cost = 0.026869907
+Epoch: 0015 cost = 0.026782614
+Learning Finished!
+Accuracy: 0.9945
+Label:  [2]
+Prediction:  [2]
+'''
